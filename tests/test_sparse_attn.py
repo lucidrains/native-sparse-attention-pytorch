57b71de78d18cb46797ca0f402041b0f42d8f971
lucidrains <lucidrains@gmail.com>
2025-02-21 14:46:37 +0000

    cover fine > compress

--- tests/test_sparse_attn.py
+++ tests/test_sparse_attn.py
@@ -9,7 +9,7 @@ from native_sparse_attention_pytorch import SparseAttention
 @pytest.mark.parametrize('use_diff_topk', (False, True))
 @pytest.mark.parametrize('seq_len', (1, 4, 31, 32, 120))
 @pytest.mark.parametrize('kv_heads', (8, 4))
-@pytest.mark.parametrize('selection_block_size', (4, 2))
+@pytest.mark.parametrize('selection_block_size', (8, 4, 2))
 def test_sparse_attn(
     use_diff_topk,
     seq_len,
